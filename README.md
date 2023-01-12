591a8e940f3c45a2f656da8c594672769cbcc247
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2023-01-12 20:06:14 +0100

    modify `web_api_via_django/settings.py` (so as to register the newly-created Django app with the Django project) + modify `web_api_via_django/urls.py` (so as to configure any HTTP requests, which are received by the Django project and have a URL starting with `/api/`, to be passed on to (= handled by) the `api_example` Django app) + add request-handling logic to the `api_example` Django app (= the `api_example/` folder)

--- README.md
+++ README.md
@@ -23,3 +23,25 @@ $ source venv/bin/activate
 
 (venv) $ python manage.py runserver
 ```
+
+```
+# Launch a second terminal instance and, in it, issue requests to the application:
+
+$ http localhost:8000/api/
+
+HTTP/1.1 200 OK
+Allow: OPTIONS, GET
+Content-Length: 26
+Content-Type: application/json
+Cross-Origin-Opener-Policy: same-origin
+Date: Thu, 12 Jan 2023 19:10:58 GMT
+Referrer-Policy: same-origin
+Server: WSGIServer/0.2 CPython/3.8.3
+Vary: Accept, Cookie
+X-Content-Type-Options: nosniff
+X-Frame-Options: DENY
+
+{
+    "message": "Hello world!"
+}
+```
--- api_example/views.py
+++ api_example/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# The Django REST Framework makes it possible to implement request-handling functions
+# in a function-based way or in a class-based way,
+# as per the following section from the framework's official documentation:
+# https://www.django-rest-framework.org/tutorial/2-requests-and-responses/#wrapping-api-views .
+# The following statement imports a symbol, which
+# makes it possible to implement request-handling functions in a function-based way.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(["GET"])
+def hello_world(request):
+    data = {
+        "message": "Hello world!",
+    }
+    return Response(data)
