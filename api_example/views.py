0b9649dfa7c398984cc741c0ef9dfea9dd632076
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2023-01-17 05:54:58 +0100

    implement `ParadigmView` and `ProgrammerView`

--- api_example/views.py
+++ api_example/views.py
@@ -5,10 +5,20 @@
 # The following statement imports a symbol, which
 # makes it possible to implement request-handling functions in a class-based way.
 from rest_framework import viewsets
-from .models import Language
-from .serializers import LanguageSerializer
+from .models import Paradigm, Language, Programmer
+from .serializers import ParadigmSerializer, LanguageSerializer, ProgrammerSerializer
+
+
+class ParadigmView(viewsets.ModelViewSet):
+    queryset = Paradigm.objects.all()
+    serializer_class = ParadigmSerializer
 
 
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+
+
+class ProgrammerView(viewsets.ModelViewSet):
+    queryset = Programmer.objects.all()
+    serializer_class = ProgrammerSerializer
