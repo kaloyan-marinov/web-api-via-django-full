0cc1ed9cb1ff462367e486e5c2eaf01b8dee3ecb
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2023-01-17 05:55:39 +0100

    register the `ParadigmView` and `ProgrammerView` with the `rest_framework.routers.DefaultRouter` instance

--- api_example/urls.py
+++ api_example/urls.py
@@ -4,7 +4,9 @@ from rest_framework import routers
 
 
 d_r = routers.DefaultRouter()
+d_r.register("paradigms", views.ParadigmView)
 d_r.register("languages", views.LanguageView)
+d_r.register("programmers", views.ProgrammerView)
 
 
 urlpatterns = [
